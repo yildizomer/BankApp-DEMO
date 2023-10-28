ad6a1dd477460be3ca6f69c8b37fd6134aa0b6a2
yildizomer <omerfarukyildiz129@gmail.com>
2023-10-28 23:24:18 +0300

    initial commit

--- proje.py
+++ proje.py
@@ -1,5 +1,5 @@
 import random
-import json
+import sqlite3
 
 class User:
     def __init__(self, username, password):
@@ -9,52 +9,75 @@ class User:
 
 class DataBase:
     def __init__(self):
-        self.file_name = 'users.json'
-        self.db_data = None
-        self.read_db()
+        self.conn = sqlite3.connect('database.db')
+        self.create_table()
 
-    def read_db(self):
-        with open(self.file_name, 'r') as json_file:
-            json_data = json.load(json_file)
-            self.db_data = json_data
+    def create_table(self):
+        cursor = self.conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                money INTEGER NOT NULL
+            )
+        ''')
+        self.conn.commit()
 
-    def transfer_money(self, sender, receiver, amount):
-        if sender['money'] < amount:
+    def transfer_money(self, sender, receiver_id, amount):
+        cursor = self.conn.cursor()
+    
+    
+        cursor.execute("SELECT money FROM users WHERE id = ?", (sender.id,))
+        sender_money = cursor.fetchone()[0]
+
+        if sender_money < amount:
             print("Yetersiz bakiye. İşlem iptal edildi.")
             return
 
-        sender['money'] -= amount
-        receiver['money'] += amount
-        self.save_db()
-        print(f"{sender['username']} tarafından {receiver['username']} kullanıcısına {amount} TL gönderildi.")
+        sender_money -= amount
+        cursor.execute("UPDATE users SET money = ? WHERE id = ?", (sender_money, sender.id))
 
-    def save_db(self):
-        with open(self.file_name, 'w') as json_file:
-            json.dump(self.db_data, json_file, indent=4)
+    
+        cursor.execute("SELECT money FROM users WHERE id = ?", (receiver_id,))
+        receiver_money = cursor.fetchone()[0]
+        receiver_money += amount
+        cursor.execute("UPDATE users SET money = ? WHERE id = ?", (receiver_money, receiver_id))
+
+        self.conn.commit()
+        print(f"{sender.username} tarafından {receiver_username} kullanıcısına {amount} TL gönderildi.")
+
+
 
     def user_signin(self, username, password):
-        for user in self.db_data['users']:
-            if user['username'] == username and user['password'] == password:
-                print("Giriş başarılı. Hoş geldiniz, {}.".format(username))
-                return user
-
-        print("Kullanıcı adı veya şifre hatalı. Lütfen tekrar deneyin.")
-        return None
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        user_data = cursor.fetchone()
+        if user_data:
+            user = User(user_data[1], user_data[2])
+            user.id = user_data[0]
+            user.money = user_data[3]
+            print("Giriş başarılı. Hoş geldiniz, {}.".format(username))
+            return user
+        else:
+            print("Kullanıcı adı veya şifre hatalı. Lütfen tekrar deneyin.")
+            return None
 
     def user_signup(self, username, password):
-        for user in self.db_data['users']:
-            if user['username'] == username:
-                print("Kullanıcı adı zaten var. Başka bir kullanıcı adı seçin.")
-                return
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            print("Kullanıcı adı zaten var. Başka bir kullanıcı adı seçin.")
+            return
 
         user = User(username, password)
-        new_user = {
-            "username": user.username,
-            "password": user.password,
-            "money": user.money
-        }
-        self.db_data['users'].append(new_user)
-        self.save_db()
+        cursor.execute("INSERT INTO users (username, password, money) VALUES (?, ?, ?)", (user.username, user.password, user.money))
+        self.conn.commit()
+        print("Kayıt başarılı. Hoş geldiniz, {}.".format(username))
+
+    def close_connection(self):
+        self.conn.close()
 
 db = DataBase()
 
@@ -67,10 +90,7 @@ Seçim:""")
             kullanici_adi = input("Kullanıcı adınızı giriniz:")
             kullanici_sifre = input("Şifrenizi giriniz:")
             user = db.user_signin(kullanici_adi, kullanici_sifre)
-            user_data = db.user_signin(kullanici_adi, kullanici_sifre)
-            if user_data is not None:
-                user = User(user_data['username'], user_data['password'])
-                user.money = user_data['money']
+            if user is not None:
                 print("""Giriş yapıldı. 
             *********************************
 
@@ -90,44 +110,46 @@ Seçim:""")
                     if a == "T":
                         add_money = int(input("Yatırmak istediğiniz miktarı giriniz: "))
                         user.money += add_money
-                        user_data['money'] += add_money
+                        cursor = db.conn.cursor()
+                        cursor.execute("UPDATE users SET money = ? WHERE id = ?", (user.money, user.id))
+                        db.conn.commit()
                         print("Yatırılan miktar: ", add_money)
                         print("Hesabınızda bulunan miktar", user.money)
-                        db.save_db()
                         continue
                     if a == "E":
+                        db.close_connection()
                         print("Çıkışınız başarıyla yapıldı! İyi günler dileriz...")
-                        break
+                        exit()
                     if a == "B":
                         money2 = int(input("Çekmek istediğiniz para miktarını giriniz: "))
                         print("Çekilen miktar: ", money2)
                         print("Hesabınızda kalan miktar", user.money)
                         if money2 > user.money:
                             print("Yalnızca hesabınızda olduğu kadar para çekebilirsiniz!")
-                        continue
-
-                        print("Çekilen tutar:", money2)
-                        user.money -= money2
-                        user_data['money'] -= money2
-                        db.save_db()
+                        else:
+                            user.money -= money2
+                            cursor = db.conn.cursor()
+                            cursor.execute("UPDATE users SET money = ? WHERE id = ?", (user.money, user.id))
+                            db.conn.commit()
+                            print("Çekilen tutar:", money2)
                         continue
                     if a == "Z":
                         receiver_username = input("Para göndermek istediğiniz kullanıcının adını giriniz: ")
-                        receiver_data = None
-                        for u_data in db.db_data['users']:
-                            if u_data['username'] == receiver_username:
-                                receiver_data = u_data
-                                break
-                        if receiver_data is None:
+                        cursor = db.conn.cursor()
+                        cursor.execute("SELECT id FROM users WHERE username = ?", (receiver_username,))
+                        receiver_id = cursor.fetchone()
+    
+                        if not receiver_id:
                             print("Alıcı kullanıcı bulunamadı.")
                         else:
+                            receiver_id = receiver_id[0]
                             transfer_amount = int(input("Göndermek istediğiniz miktarı giriniz: "))
-                            db.transfer_money(user_data, receiver_data, transfer_amount)
+                            db.transfer_money(user, receiver_id, transfer_amount)
+
 
     elif islem1 == "K":
         kullanici_adi = input("Kullanıcı adınızı giriniz:")
         kullanici_sifre = input("Şifrenizi giriniz:")
         db.user_signup(kullanici_adi, kullanici_sifre)
     else:
-        print("Geçerli işlem seçiniz...")
-db.save_db
+        print("Geçerli işlem seçiniz.")
